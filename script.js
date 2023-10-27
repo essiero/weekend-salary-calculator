a8e23b6ec6cef20959b3799a5c784a8d44255079
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-27 17:30:30 -0500

    filled out handleSubmit function to push form input into the corresponding table box

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ let totalMonthly = 0;
 function handleSubmit(event){
     event.preventDefault();
     console.log('Submit function is working')
-    
+    let firstName = document.getElementById('firstName').value;
+    document.getElementById('firstName').value = '';
+    let lastName = document.getElementById('lastName').value;
+    document.getElementById('lastName').value = '';
+    let IDNumber = document.getElementById('IDnumber').value;
+    document.getElementById('IDnumber').value = '';
+    let title = document.getElementById('title').value;
+    document.getElementById('title').value = 0;
+    let annualSalary = document.getElementById('annualSalary').value;
+    document.getElementById('annualSalary').value = 0;
+    tableBody.innerHTML +=
+    `<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${IDNumber}</td>
+    <td>${title}</td>
+    <td>${annualSalary}</td>
+    <td><button onclick="deleteEmployee(event)">❌</button></td>
+    </tr>`
+}
+
+function deleteEmployee(event){
+    console.log(`deleteEmployee function is running`);
+    event.target.parentElement.parentElement.remove();
 }
 
 

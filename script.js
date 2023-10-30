c2bc0e2fbc3257de6b501cd1a81a0e3dfe050ce3
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-10-30 14:00:06 -0500

    changed js to add over-budget class to footer instead of using an id for the span element

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ onReady();
 
 // to update Counter: use innerHTML
 let totalMonthly = 0;
-let employeeArray = [];
-let employeeIndex = -1;
+// let employeeArray = [];
+// let employeeIndex = 0;
 
+/* NOTES FROM MATT'S MONDAY REVIEW
+- README.md is a place for you to document your project process.
+document.querySelector('h1').classList.add('pink-text')
+- if you don't defer the JS file, it will mess up the JS code that's supposed to apply to html bc it hasn't created the html yet.
+- you can move a line of code with option + arrow
+- command bracket to indent a line of code right or left
+ */
 
 function handleSubmit(event){
-    event.preventDefault();
+    event.preventDefault(); // this is so that clicking submit doesn't refresh the page.
     console.log('Submit function is working')
     let firstName = document.getElementById('firstName').value;
     document.getElementById('firstName').value = '';
@@ -19,7 +26,8 @@ function handleSubmit(event){
     document.getElementById('title').value = '';
     let annualSalary = document.getElementById('annualSalary').value;
     document.getElementById('annualSalary').value = '';
-    tableBody.innerHTML +=
+    tableBody.innerHTML += // the reason you can do this with tableBody is that there is only one; BUT, if you add another table body at some point,
+    // it would push into both. So you can add an id to your specific table body and use: document.getElementByID('whatever-the-id-is').innerHTML
         // add onclick function trigger to delete employee object from array at employee index number! somehow!! 
     `<tr>
     <td>${firstName}</td>
@@ -30,46 +38,70 @@ function handleSubmit(event){
     <td><button onclick="deleteEmployee(event)">❌</button></td>
     </tr>`;
     totalMonthly += annualSalary/12;
+   /* FROM MATT'S LECTURE:
+   let annualSalary = 0;
+   let totalAnnualSalary += Number (totalAnnualSalary)
+// Selects footer, accesses its CSS class list and adds a new 'over-budget' CSS class to it
+    document.querySelector('footer').classList.add('over-budget)
+
+
+
+ */
+    // let employeeAdded = {
+    //     name: firstName + ' ' + lastName,
+    //     ID: IDNumber,
+    //     title: title,
+    //     monthlySalary: annualSalary/12,
+    //     indexNumber: employeeIndex ++
+    // }
+    // employeeArray.push(employeeAdded);
+    // console.log(`Employee array: `, employeeArray)
+    // console.log(`Employee added's index:`, employeeIndex);
+//     function updateBudget(array, object){
+//         for (employee of array){
+//             for (index in employee){
+//             totalMonthly += `${array[index].monthlySalary}`;
+//         console.log(array[i].indexNumber);
+//         console.log(totalMonthly)
+//             }
+//         }
+//         console.log('Total monthly budget: ', totalMonthly);
+// updateBudget(employeeArray);
     let USDollar = new Intl.NumberFormat('en-us', {
         style: 'currency',
         currency: 'USD',
     });
     document.getElementById('number').textContent = `${USDollar.format(totalMonthly)}`;
     if(totalMonthly>20000){
-        document.getElementById('number').style.color="red";
+        // document.getElementById('number').style.color="red";
+        document.querySelector('footer').classList.add('over-budget');
     }
-    // let employeeAdded = {
-    //     name: firstName + ' ' + lastName,
-    //     ID: IDNumber,
-    //     title: title,
-    //     monthlySalary: annualSalary/12
-    //     indexNumber: employeeIndex ++;
-    // }
-    // employeeArray.push(employeeAdded);
-    // console.log(employeeIndex);
-    // salaryCalculator(employeeArray);
-    
-}
+    }
 
-function salaryCalculator(array){
-    for (employee of array){
-        totalMonthly += employee.monthlySalary;
-    }
-        let USDollar = new Intl.NumberFormat('en-us', {
-        style: 'currency',
-        currency: 'USD',
-    });
-    console.log('Total monthly: ', totalMonthly);
-    document.getElementById('number').textContent = `${USDollar.format(totalMonthly)}`;
-    if(totalMonthly>20000){
-        document.getElementById('number').style.color="red";
-}
-}
+// function salaryCalculator(array){
+//     for (employee of array){
+//         totalMonthly += employee.monthlySalary;
+//     }
+//         let USDollar = new Intl.NumberFormat('en-us', {
+//         style: 'currency',
+//         currency: 'USD',
+//     });
+//     console.log('Total monthly: ', totalMonthly);
+//     document.getElementById('number').textContent = `${USDollar.format(totalMonthly)}`;
+//     if(totalMonthly>20000){
+//         document.getElementById('number').style.color="red";
+// }
+// }
 
 function deleteEmployee(event){
     console.log(`deleteEmployee function is running`);
     event.target.parentElement.parentElement.remove();
-    salaryCalculator(employeeArray);
+// you can also go up until you find a table row!
+// selects the button that was clicked (event.target)
+// travels upward through the DOM structure to find the first <tr> element: .closest('tr')
+// event.target.closest('tr').remove()
+
+    // salaryCalculator(employeeArray);
 // I want this to ALSO delete that employee from the employeeArray!
 
 }
